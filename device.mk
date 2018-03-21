1f55f804406bfd1f6cdbc7a160f67b11e529a2db
klozz <klozz707@gmail.com>
2018-03-21 05:43:01 -0400

    mido: remove icon from doze package * Remove LockscreenRotation(? * show activity indicators in the status bar * dont forget to build chargeonlymode
    Change-Id: I369cd14f9fe7751e0c963d162cd8aed6a6d8c861
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:system/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
 
+# Healthd
+PRODUCT_PACKAGES += \
+    chargeonlymode
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
